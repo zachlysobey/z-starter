bbbb594ea74892eb9f5cd01af3989b27da6a74ed
Zach Lysobey <zlysobey@gmail.com>
2019-10-18 14:10:44 -0400

    👷 add babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
 }
