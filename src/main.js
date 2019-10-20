2e76757dde90c3e8c9f1b193cd94daf9142d7da0
Zach Lysobey <zlysobey@gmail.com>
2019-10-19 22:41:04 -0400

    👋 Hello, Koa

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello, World')
+const Koa = require('koa')
+
+const app = new Koa()
+
+app.use(async ctx => {
+    ctx.body = 'Hello, Koa!'
+})
+
+console.log('server listening on http://localhost:3000')
+app.listen(3000)
