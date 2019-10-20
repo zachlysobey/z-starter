30054c036fb727f85736641f5f22f29a6ed1f4b7
Zach Lysobey <zlysobey@gmail.com>
2019-10-19 23:02:29 -0400

    ✨ hook up koa-router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
 
+const { router } = require('./router')
+
 const app = new Koa()
 
 app.use(logger())
-
-app.use(async ctx => {
-    ctx.body = 'Hello, Koa!'
-})
+    .use(router.routes())
+    .use(router.allowedMethods())
 
 console.log('server listening on http://localhost:3000')
 app.listen(3000)
